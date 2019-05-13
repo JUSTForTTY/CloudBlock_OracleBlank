8979c7f6930fea8dab0a0e8bf51cb178153419d5
JUSTForTTY <1547970885@qq.com>
2019-05-13 19:29:44 +0800

    zz

--- src/app/core/startup/startup.service.ts
+++ src/app/core/startup/startup.service.ts
@@ -11,10 +11,11 @@ import {
 import { ACLService } from '@delon/acl';
 import { TranslateService } from '@ngx-translate/core';
 import { I18NService } from '../i18n/i18n.service';
-
+import { JwtService } from 'ngx-block-core';
 import { NzIconService } from 'ng-zorro-antd';
 import { ICONS_AUTO } from '../../../style-icons-auto';
 import { ICONS } from '../../../style-icons';
+import {UserService} from './../service/user.service'
 
 /**
  * 用于应用启动时
@@ -31,6 +32,8 @@ export class StartupService {
     private aclService: ACLService,
     private titleService: TitleService,
     private httpClient: HttpClient,
+    private jwtService:JwtService,
+    private userService:UserService
   ) {
     iconSrv.addIcon(...ICONS_AUTO, ...ICONS);
   }
@@ -65,7 +68,7 @@ export class StartupService {
             // ACL：设置权限为全量
             this.aclService.setFull(true);
             // 初始化菜单
-           
+            this.userService.loadMenu(this.userService.getCurrentUser());
             // 设置页面标题的后缀
             this.titleService.default = '';
             this.titleService.suffix = res.app.name;

24698d2d6c0e09e0fc8e2202682dd9a93e62a321
tty <1547970885@qq.com>
2019-06-28 19:28:16 +0800

    zz

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,7 +3,7 @@ export const environment = {
     SERVER_NAME:`lh_mes`,
     chore: false,
     SERVER_URL: `http://172.16.8.107/cloudblock_lhoracle/`,
-    DATA_SERVER_URL:`http://172.16.8.107/cloudblock_lhoracle/`,
+    DATA_SERVER_URL:`http://172.16.8.107:8082/cloudblock_datapull/`,
     production: true,
     hmr: false,
     useHash: false,

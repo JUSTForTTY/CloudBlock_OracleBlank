154e648ae3f26dae3bc9df2ce90134a1135791a6
gury <zbrongyaozb@163.com>
2020-03-30 11:32:01 +0800

    gury 头部

--- src/app/routes/dashboard/workshop-board/workshop-board.module.ts
+++ src/app/routes/dashboard/workshop-board/workshop-board.module.ts
@@ -3,12 +3,12 @@ import { CommonModule } from '@angular/common';
 import { WorkshopBoardRoutingModule } from "./workshop-board-routing.module";
 import { YieldProductlineComponent } from './yield-productline/yield-productline.component';
 import { WorkshopBoardComponent } from './workshop-board.component';
-
+import { SharedModule } from '@shared/shared.module';
 @NgModule({
   declarations: [YieldProductlineComponent, WorkshopBoardComponent],
   imports: [
     CommonModule,
-    WorkshopBoardRoutingModule
+    WorkshopBoardRoutingModule,SharedModule
   ]
 })
 export class WorkshopBoardModule { }

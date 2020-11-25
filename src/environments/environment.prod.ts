089c642b5285778d776af91774b52386d54a813b
lhmes <1547970885@qq.com>
2020-11-25 18:25:30 +0800

    zz

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,11 @@
  
 export const environment = {
-    HOME_URL:`http://172.16.8.112:8091/`,
+    HOME_URL:`http://39:98.81.218:8091/`,
     SERVER_NAME:`lh_mes`,
     chore: false,
-    SERVER_URL: `http://172.16.8.112:81/cloudblock_lhoracle/`,
-    RESOURCE_SERVER_URL: `http://172.16.8.112:81/`,
-    DATA_SERVER_URL:`http://172.16.8.107:8082/cloudblock_datapull/`,
+    SERVER_URL: `http://39:98.81.218/cloudblock_lhoracle/`,
+    RESOURCE_SERVER_URL: `http://39:98.81.218:81/`,
+    DATA_SERVER_URL:`h`,
     production: true,
     hmr: false,
     useHash: false,

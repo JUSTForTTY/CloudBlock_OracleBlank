bf0a08b5aa530499e5376713357c254c0ef7135c
zbrongyaozb <1505749060@qq.com>
2021-03-09 10:24:30 +0800

    gury

--- src/app/routes/dashboard/rps-board/rps-board.component.ts
+++ src/app/routes/dashboard/rps-board/rps-board.component.ts
@@ -9,7 +9,7 @@ import { fromEvent as observableFromEvent, of as observableOf } from 'rxjs';
 })
 export class RpsBoardComponent implements OnInit {
   /** 标准 */
-  standard={
+  standard = {
     complete: {
       good: 95,
       bad: 75
@@ -23,45 +23,31 @@ export class RpsBoardComponent implements OnInit {
   allData: {
     title: string;
     data: Data[];
+    isLoading?: boolean;
   }[][] = [];
   topData: {
     title: string;
     data: Data[];
+    isLoading?: boolean;
   }[] = [];
   bottomData: {
     title: string;
     data: Data[];
+    isLoading?: boolean;
   }[] = [];
   fontSizeTitle1 = 42;//一级标题
   fontSizeTitle2 = 24;//二级标题
   tableSize: 'middle' | 'small' = 'middle';
   private nowTimeTimer;
   nowTime = Date.now();
+
+  private dataTimer;
   constructor() {
 
   }
 
   ngOnInit() {
-    // 模拟数据
-    this.topData.push({
-      title: 'SMT达成率',
-      data: getTestData()
-    })
-    this.topData.push({
-      title: 'WAVE达成率',
-      data: getTestData()
-    })
-    this.bottomData.push({
-      title: '选焊达成率',
-      data: getTestData()
-    })
-    this.bottomData.push({
-      title: 'COATING达成率',
-      data: getTestData(),
-      // data:[]
-    })
-    this.allData.push(this.topData);
-    this.allData.push(this.bottomData)
+
     if (window.screen.height <= 900) {
       this.fontSizeTitle1 = 32;//一级标题
       this.fontSizeTitle2 = 18;//二级标题
@@ -69,6 +55,53 @@ export class RpsBoardComponent implements OnInit {
     }
     this.autoSize();
     this.nowTimeTimer = setInterval(() => this.nowTime = Date.now(), 1000)
+
+    if (this.dataTimer) {
+      clearInterval(this.dataTimer);
+    }
+    this.getAllData();
+    this.dataTimer = setInterval(() => {
+      this.getAllData();
+    }, 10 * 60 * 1000)
+
+  }
+  getAllData() {
+    console.log('getAllData')
+    this.topData = [];
+    this.bottomData = [];
+    this.allData = [];
+    this.topData.push({
+      title: 'SMT达成率',
+      data: getTestData(),
+      isLoading:true
+    })
+    this.topData.push({
+      title: 'WAVE达成率',
+      data: getTestData(),
+      isLoading:true
+    })
+    this.bottomData.push({
+      title: '选焊达成率',
+      data: getTestData(),
+      isLoading:true
+
+    })
+    this.bottomData.push({
+      title: 'COATING达成率',
+      data: getTestData(),
+      isLoading:true
+      // data:[]
+    })
+    this.allData.push(this.topData);
+    this.allData.push(this.bottomData);
+    setTimeout(() => {
+      // 模拟数据
+      for (const iterator of this.allData) {
+        for (const data of iterator) {
+          data.isLoading=false;
+        }
+      }
+    }, 1);
 
   }
   autoSize() {
@@ -91,6 +124,9 @@ export class RpsBoardComponent implements OnInit {
   ngOnDestroy() {
     if (this.nowTimeTimer) {
       clearInterval(this.nowTimeTimer);
+    }
+    if (this.dataTimer) {
+      clearInterval(this.dataTimer);
     }
   }
 }

4b2501163b8bc47d4a21d125ecc1600bf8a3585d
zbrongyaozb <1505749060@qq.com>
2021-04-28 13:14:38 +0800

    fyrt

--- src/app/routes/dashboard/ad-board/depart-avg/depart-avg.component.ts
+++ src/app/routes/dashboard/ad-board/depart-avg/depart-avg.component.ts
@@ -1,15 +1,15 @@
 import { Component, Input, OnInit } from '@angular/core';
 const sourceData = [
-  { name: 'WEEK1', 'PE': 5.5, 'EE': 4.5, 'TE': 3.9, 'AE': 3.1, 'IT': 4.7, 'JE': 2.0 },
+  { name: 'WEEK1', 'PE': 5.5, 'EE': 4.5, 'TE': 3.9, 'AE': 3.1,  'JE': 2.0 },
   { name: 'WEEK2', 'PE': 3.3, 'EE': 2.3, 'TE': 3.4, 'AE': 4.9, 'IT': 5.2, 'JE': 3.5 },
-  { name: 'WEEK3', 'PE': 4.2, 'EE': 3.2, 'TE': 4.5, 'AE': 6.6, 'IT': 4.7, 'JE': 3.3 },
+  { name: 'WEEK3', 'PE': 4.2, 'EE': 3.2,  'AE': 6.6, 'IT': 4.7, 'JE': 3.3 },
   { name: 'WEEK4', 'PE': 5.2, 'EE': 2.8, 'TE': 2.2, 'AE': 1.3, 'IT': 5.4, 'JE': 2.0 }
 ];
 const Fields = ['PE', 'EE', 'TE', 'AE', 'IT', 'JE']
 const sourceData2 = [
   { name: 'WEEK1', 'PE': 18, 'EE': 28, 'TE': 12, 'AE': 34, 'IT': 21, 'JE': 20 },
-  { name: 'WEEK2', 'PE': 12, 'EE': 23, 'TE': 32, 'AE': 21, 'IT': 23, 'JE': 35 },
-  { name: 'WEEK3', 'PE': 18, 'EE': 32, 'TE': 12, 'AE': 26, 'IT': 12, 'JE': 33 },
+  { name: 'WEEK2', 'PE': 12, 'EE': 23, 'TE': 32, 'AE': 21, 'IT': 23,  },
+  { name: 'WEEK3', 'PE': 18,  'TE': 12, 'AE': 26, 'IT': 12, 'JE': 33 },
   { name: 'WEEK4', 'PE': 14, 'EE': 28, 'TE': 22, 'AE': 21, 'IT': 32, 'JE': 20 }
 ];
 const Fields2 = ['PE', 'EE', 'TE', 'AE', 'IT', 'JE']
@@ -99,6 +99,24 @@ export class DepartAvgComponent implements OnInit {
       offsetX: -15
     };
   }
+
+  color = ['name', function (name) {
+    console.log('name',name)
+    switch (name) {
+      case 'WEEK1':
+        return '#3aa1ff'
+        case 'WEEK2':
+        return '#4ecb73'
+        case 'WEEK3':
+        return '#fcce72'
+        case 'WEEK4':
+        return '#8543e0'
+    
+      default:
+        break;
+    }
+    return null;
+  }]
 
   // start2:any={ '部门': 'PE', '总数': 5 };
   start2: any = (xScale, yScale) => {
